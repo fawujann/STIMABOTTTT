67d538d241f0b060d4ec5798d17caba4e3d9f697
fawujann <mfauzannaufal29@gmail.com>
2025-06-01 21:28:16 +0700

    bot

---
+++ run-bots.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python main.py --logic Keledai --email=test@email.com --name=KeledaiRakus --password=123456 --team etimo"
+start cmd /c "python main.py --logic rakus --email=test1@email.com --name=RakusBanget --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test2@email.com --name=Random1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test3@email.com --name=Random2 --password=123456 --team etimo"
